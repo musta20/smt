07a60056d8ba7c9f091c7caaa288088babf5123a
musta20 <musta20@users.noreply.github.com>
2024-07-02 08:06:43 +0000

    Dusting

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.'.config('app.domain')],
+    'allowed_origins' => ['*.' . config('app.domain')],
 
     'allowed_origins_patterns' => [],
 
